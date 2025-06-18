136044bb796d3aaa01d2860641af43c66b09ffa0
JustBru00 <justbru00@gmail.com>
2025-06-18 19:59:31 -0400

    Revert "Adds lapreset to the database for during initialization (I'm really not sure how I didn't find this yet)"
    This reverts commit 35f82d788cf7034ee241a391dfa61eac0dfe3ccd.

--- src/main/java/me/makkuusen/timing/system/database/MySQLDatabase.java
+++ src/main/java/me/makkuusen/timing/system/database/MySQLDatabase.java
@@ -248,7 +248,6 @@ public class MySQLDatabase implements TSDatabase, EventDatabase, TrackDatabase, 
                       `maxDrivers` int(11) DEFAULT NULL,
                       `lonely` tinyint(1) NOT NULL DEFAULT '0',
                       `canReset` tinyint(1) NOT NULL DEFAULT '0',
-                      `lapReset` tinyint(1) NOT NULL DEFAULT '0',
                       `isRemoved` tinyint(1) NOT NULL DEFAULT '0',
                       PRIMARY KEY (`id`)
                     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;""");
