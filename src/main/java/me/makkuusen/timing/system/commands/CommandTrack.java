389c52ee4fb034556f58f2550090e3b88303b3c7
M2B5 <mbowers2205@gmail.com>
2025-08-23 22:34:30 +0100

    Cosmetic improvement

--- src/main/java/me/makkuusen/timing/system/commands/CommandTrack.java
+++ src/main/java/me/makkuusen/timing/system/commands/CommandTrack.java
@@ -159,7 +159,7 @@ public class CommandTrack extends BaseCommand {
             Integer customModeId = track.getCustomBoatUtilsModeId();
             CustomBoatUtilsMode bume = TimingSystem.getTrackDatabase().getCustomBoatUtilsModeFromId(customModeId);
             if (bume != null) {
-                Text.send(commandSender, Info.TRACK_BOATUTILS_MODE, "%mode%", "CUSTOM");
+                Text.send(commandSender, Info.TRACK_BOATUTILS_MODE, "%mode%", "CUSTOM (" + bume.getName() + ")");
             } else {
                 Text.send(commandSender, Info.TRACK_BOATUTILS_MODE, "%mode%", "VANILLA");
             }
